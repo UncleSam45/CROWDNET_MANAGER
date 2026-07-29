2ed902f9020bbe93a1bd631ac415677c25a1a198
Sam V. <unclesam450@pm.me>
2026-07-29 19:18:03 -0400

    Redesign tasks tab as execution board

--- preload.js
+++ preload.js
@@ -5,4 +5,5 @@ contextBridge.exposeInMainWorld('crowdnet', Object.freeze({
   authenticate: input => ipcRenderer.invoke('bridge:authenticate', input),
   restore: () => ipcRenderer.invoke('bridge:read'),
   save: payload => ipcRenderer.invoke('bridge:write', payload),
+  matchCompletedPullRequests: projectName => ipcRenderer.invoke('github:match-completed', { projectName }),
 }));
